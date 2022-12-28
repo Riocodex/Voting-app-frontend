457165b43259e45e05b25f11634994ebe468f8aa
Rio <onwukachibike@gmail.com>
2022-12-29 00:53:31 +0100

    updated walletconnection

--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,70 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
+import { ethers } from "ethers";
+
+import React, { useEffect , useState } from 'react'
+
+
 import VoterCard from '../components/voterCard'
 import StartElection from '../components/StartElection'
 import Register from '../components/register'
 
+import abi from '../constants/abi.json'
 
 
 export default function Home() {
+
+  let contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3"
+  const contractABI = abi.abi
+
+  //web3 states
+  const [currentAccount, setCurrentAccount] = useState("");
+ 
+
+      // Wallet connection logic
+  const isWalletConnected = async () => {
+    try {
+      const { ethereum } = window;
+
+      const accounts = await ethereum.request({method: 'eth_accounts'})
+      console.log("accounts: ", accounts);
+
+      if (accounts.length > 0) {
+        const account = accounts[0];
+        console.log("wallet is connected! " + account);
+      } else {
+        console.log("make sure MetaMask is connected");
+      }
+    } catch (error) {
+      console.log("error: ", error);
+    }
+  }
+  const connectWallet = async () => {
+    try {
+      const {ethereum} = window;
+
+      if (!ethereum) {
+        console.log("please install MetaMask");
+      }
+
+      const accounts = await ethereum.request({
+        method: 'eth_requestAccounts'
+      });
+
+      setCurrentAccount(accounts[0]);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+ 
+
+  useEffect(() => {
+   
+    isWalletConnected();
+    
+  },[])
   return (
     <>
        <nav className=" bg-blue-800 p-5 border-b-2 flex flex-row ">
@@ -21,13 +78,19 @@ export default function Home() {
            </div>
             <div className="ml-auto mt-2 py-2 px-4">
           
+            {currentAccount ? (
             <button
-                    type="button"
-                    className='bg-blue-500 py-2 px-4 rounded-md text-white font-bold text-xl hover:bg-blue-700 '
-                    
-                >
-                    Connect
-                </button>
+            
+            className='bg-blue-500 py-2 px-4 rounded-md text-white font-bold text-xl hover:bg-blue-700'>Wallet Connected!</button>
+        ) : (
+          <button
+          onClick={connectWallet}
+          type="button"
+          className='bg-blue-500 py-2 px-4 rounded-md text-white font-bold text-xl hover:bg-blue-700'   
+      >
+          Connect
+      </button>
+        )}
             </div>
         </nav>
       <div className='py-10 px-10 '>
