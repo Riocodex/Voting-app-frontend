6dd63f1348bf89e023f69b2151713d9fc5952515
Rio <onwukachibike@gmail.com>
2022-12-29 00:26:31 +0100

    created project

---
+++ components/register.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Register = () => {
+  return (
+    <div>
+    <div className='h-screen flex justify-center items-center py-20 px-20 '>
+<div className='voter-back flex flex-col flex justify-center items-center border-green-800 py-20 shadow-xl rounded-md'>
+
+</div>
+<div className='flex flex-col flex justify-center items-center border-blue-800 border-8 border-dotted px-6 py-20 h-full w-1/2 rounded-md shadow-xl'>
+<label className='font-bold text-2xl'>Full Name</label>
+<input class="shadow-md m-2 px-20 py-5 rounded-lg border-gray-200 border-2 text-gray-500 text-xl font-semibold justify-center items-center placeholder-indigo-300" type="text"  placeholder="Enter your full name"/>
+<button 
+
+class="bg-blue-800 rounded-lg text-white py-5 px-6 font-bold text-xl mt-2">Register</button>
+</div>
+</div>
+</div>
+  )
+}
+
+export default Register
---
+++ components/voterCard.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const VoterCard = () => {
+  return (
+    <div className='bg-blue-800 w-1/3 h-full mr-6 py-4  rounded-xl hover:shadow-2xl '>
+    <div>
+    <h1 className='flex justify-center items-center text-2xl text-white font-bold uppercase '>NAME</h1>
+        <h1 className='flex justify-center items-center text-white font-bold uppercase mt-1'>Onwuka Rosario</h1>
+    </div>
+     <div>
+     <h1 className='flex justify-center items-center text-2xl text-white font-bold uppercase mt-3'>VOTES</h1>
+        <p className='flex justify-center items-center text-sm text-white font-bold uppercase mt-1'>583</p>
+     </div>
+       </div>
+  )
+}
+
+export default VoterCard
