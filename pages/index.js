06c66e188923b717f21abb9d713f642edfb04bb4
Rio <onwukachibike@gmail.com>
2022-12-30 17:23:43 +0100

    updated props

--- pages/index.js
+++ pages/index.js
@@ -117,7 +117,7 @@ export default function Home() {
       
             <VoterCard
               key={item.id}
-              {...item}
+              item={item}
               
             />
             
