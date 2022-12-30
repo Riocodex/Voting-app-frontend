0c92711b277c7ea347654cf966a9edf1adc7f47f
Rio <onwukachibike@gmail.com>
2022-12-30 17:24:56 +0100

    debugged

--- components/voterCard.js
+++ components/voterCard.js
@@ -6,11 +6,11 @@ const VoterCard = ({item}) => {
     <div className='bg-blue-800 w-1/3 h-full mr-6 py-4  rounded-xl hover:shadow-2xl '>
     <div>
     <h1 className='flex justify-center items-center text-2xl text-white font-bold uppercase '>NAME</h1>
-        <h1 className='flex justify-center items-center text-white font-bold uppercase mt-1'>{props.name}</h1>
+        <h1 className='flex justify-center items-center text-white font-bold uppercase mt-1'>{item.name}</h1>
     </div>
      <div>
      <h1 className='flex justify-center items-center text-2xl text-white font-bold uppercase mt-3'>VOTES</h1>
-        <p className='flex justify-center items-center text-sm text-white font-bold uppercase mt-1'>{props.numVotes.toString()}</p>
+        <p className='flex justify-center items-center text-sm text-white font-bold uppercase mt-1'>{item.numVotes.toString()}</p>
      </div>
        </div>
   )
